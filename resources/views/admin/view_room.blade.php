9e6e751861aa4dea2cb281bdc0e6275afff09831
voxuanngoc <voxuanngoc140405@gmail.com>
2024-11-21 09:21:56 +0700

    Display data in Laravel Admin Panel

---
+++ resources/views/admin/view_room.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <style type="text/css">
+    .table_deg
+    {
+        border: 2px solid white;
+        margin: auto;
+        width: 50%;
+        text-align: center;
+        margin-top: 40px;
+    }
+    .th_deg
+    {
+        background-color: skyblue;
+        padding: 15px;
+
+    }
+    tr
+    {
+        border: 3px solid white;
+    }
+    td 
+    {
+        padding: 10px;
+    }
+  </style>
+  <body>
+    @include('admin.header')
+    
+    @include('admin.sidebar')
+
+      <!-- Sidebar Navigation end-->
+
+      <div class="page-content">
+    <div class="page-header">
+      <div class="container-fluid">
+    <table class="table_deg">
+<tr>
+    <th class="th_deg">Room Title</th>
+    <th class="th_deg">Description</th>
+    <th class="th_deg">Price</th>
+    <th class="th_deg">Wifi</th>
+    <th class="th_deg">Room Type</th>
+    <th class="th_deg">Image</th>
+
+</tr>
+@foreach($data as $data)
+<tr>
+    <td>{{$data->room_title}}</td>
+    <td>{!! Str::limit($data->description,150) !!}</td>
+    <td>{{$data->price}}$</td>
+    <td>{{$data->wifi}}</td>
+    <td>{{$data->room_type}}</td>
+    <td>
+        <img width="60" src="room/{{$data->image}}" alt="">
+    </td>
+</tr>
+
+    </table>a
+</div>
+</div>
+</div>
+
+
+      
+        @include('admin.footer')
+  </body>
+</html>
