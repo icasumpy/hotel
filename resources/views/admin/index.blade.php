b55ae7e5f845e8b0fe6b274d51542e91bf766717
Phan Thi Khanh Ly <phanthikhanhly2026520@gmail.com>
2024-11-25 14:22:23 +0700

    ly

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,8 +10,20 @@
 
       <!-- Sidebar Navigation end-->
 
-      @include('admin.body')
+      <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
+
+            <div style="text-align: center;"> <!-- Sử dụng CSS để căn giữa -->
+                <h1 style="font-size: 30px; font-weight: bold">
+                    Welcome {{ $data->name ?? 'Admin' }} <!-- Kiểm tra nếu $data->name không có giá trị thì hiển thị 'Khách' -->
+                </h1>
+            </div>
+
+            </div>
+        </div>
+    </div>
       
-        @include('admin.footer')
+    @include('admin.footer')
   </body>
 </html>
